0cb68752b29509072a23b0a9785d80808f88a3d6
shk4548 <shk4548@gmail.com>
2022-04-07 09:52:21 +0900

    config

---
+++ services/service-emaillist/src/main/resources/script/launch.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+APP_NAME=service-emaillist
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+PID=$(ps -ef | grep java | grep $APP_NAME.jar | awk '{print $2}')
+
+if  [ ! -z "$PID" ] 
+then
+	echo "stopping [$APP_NAME]"
+	kill -9 $PID
+	sleep 10
+fi
+
+echo "starting [$APP_NAME]"
+cd $SCRIPT_DIR
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APP_NAME.jar >> $SCRIPT_DIR/launch.log &
