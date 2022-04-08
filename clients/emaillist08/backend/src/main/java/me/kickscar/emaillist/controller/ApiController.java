01d6cfb46d34b2d4c527c1e16b599f99c1595434
shk4548 <shk4548@gmail.com>
2022-04-08 10:37:52 +0900

    mysite08

--- clients/emaillist08/backend/src/main/java/me/kickscar/emaillist/controller/ApiController.java
+++ clients/emaillist08/backend/src/main/java/me/kickscar/emaillist/controller/ApiController.java
@@ -3,7 +3,6 @@ package me.kickscar.emaillist.controller;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +19,10 @@ import me.kickscar.emaillist.vo.EmaillistVo;
 @Slf4j
 @RestController
 public class ApiController {
-	
+	//
+	//@LoadBalanced
+	//
 	@Autowired
-	@LoadBalanced
 	private RestTemplate restTemplate;
 	
 	@GetMapping("/api")
@@ -30,7 +30,10 @@ public class ApiController {
 		
 		log.info("Request[GET /api]");
 		
-		EmaillistVo[] result = restTemplate.getForObject("http://service-emaillist/api?kw="+keyword, EmaillistVo[].class);
+		// GateWay 사용
+		//EmaillistVo[] result = restTemplate.getForObject("http://service-emaillist/api?kw="+keyword, EmaillistVo[].class);
+		//
+		EmaillistVo[] result = restTemplate.getForObject("http://localhost:5555/emaillist/api?kw="+keyword, EmaillistVo[].class);
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
@@ -39,7 +42,11 @@ public class ApiController {
 	
 	@PostMapping("/api")
 	public ResponseEntity<JsonResult> create(@RequestBody EmaillistVo vo) {
-		EmaillistVo result = restTemplate.postForObject("http://service-emaillist/api", vo, EmaillistVo.class);
+		
+		// GateWay 사용
+		//EmaillistVo result = restTemplate.postForObject("http://service-emaillist/api", vo, EmaillistVo.class);
+		//
+		EmaillistVo result = restTemplate.postForObject("http://localhost:5555/emaillist/api", vo, EmaillistVo.class);
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
